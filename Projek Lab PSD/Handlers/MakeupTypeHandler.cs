db1fa4d35f2dcfa5fd5102664db27349309243c3
Raffi Darmawan <raffidarmawan0411@gmail.com>
2024-06-03 21:02:48 +0700

    mau mati

---
+++ Projek Lab PSD/Handlers/MakeupTypeHandler.cs
@@ -0,0 +1,32 @@
+﻿using Projek_Lab_PSD.Models;
+using Projek_Lab_PSD.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projek_Lab_PSD.Handlers
+{
+    public class MakeupTypeHandler
+    {
+        public void DeleteMakeupType(int MakeupTypeID)
+        {
+            MakeupTypeRepository makeupTypeRepo = new MakeupTypeRepository();
+            MakeupRepository makeupRepo = new MakeupRepository();
+            
+            MakeupType makeupType = makeupTypeRepo.GetMakeupTypeByID(MakeupTypeID);
+
+            if(makeupType == null )
+            {
+                return;
+            }
+
+            if (makeupType.Makeups.Count > 0)
+            {
+                makeupRepo.DeleteMakeup(makeupType.Makeups.ToList());
+            }
+
+            makeupTypeRepo.DeleteMakeupTypeByID(MakeupTypeID);
+        }
+    }
+}
--- Projek Lab PSD/Views/Register.aspx.cs
+++ Projek Lab PSD/Views/Register.aspx.cs
@@ -1,4 +1,6 @@
-﻿using Projek_Lab_PSD.Models;
+﻿using Projek_Lab_PSD.Controllers;
+using Projek_Lab_PSD.Handlers;
+using Projek_Lab_PSD.Models;
 using Projek_Lab_PSD.Repositories;
 using System;
 using System.Collections.Generic;
@@ -21,96 +23,19 @@ namespace Projek_Lab_PSD.Views
         {
             String username = UsernameTB.Text;
             String password = PasswordTB.Text;
-            String confirmedPassword = ConfirmPassTB.Text;
+            String confirmPassword = ConfirmPassTB.Text;
             String email = EmailTB.Text;
-            DateTime DOB = Convert.ToDateTime(DateOfBirthTB.Text);
-        
-            if(username == "")
-            {
-                ErrorLbl.Text = "Username may not be empty!";
-            }
-            else if(username.Length <= 5 && username.Length >= 15)
-            {
-                ErrorLbl.Text = "Username must be between 5 and 15 characters!";
-            }
-            else if(email == "")
-            {
-                ErrorLbl.Text = "Email may not be empty!";
-            }
-            else if (!email.EndsWith(".com"))
-            {
-                ErrorLbl.Text = "Enter a valid email!";
-            }
-            else if (GenderList.SelectedIndex == -1)
-            {
-                ErrorLbl.Text = "Gender must be selected!";
-            }
-            else if(password == "")
-            {
-                ErrorLbl.Text = "Password may not be empty!";
-            }
-            else if (!IsAlphanumeric(password))
-            {
-                ErrorLbl.Text = "Password must be alphanumeric!";
-            }
-            else if(confirmedPassword != password)
-            {
-                ErrorLbl.Text = "Confirmed Password must match!";
-            }
-            else if (DOB.Equals(""))
-            {
-                ErrorLbl.Text = "Date of birth must be filled!";
-            }
-            else
-            {
-                ErrorLbl.Text = "User Registered Successfully!";
-                ErrorLbl.ForeColor = System.Drawing.Color.Green;
-                RegisterUser();
-            }
-        }
-        public int GenerateUserID()
-        {
-            UserRepository userRepo = new UserRepository();
+            String DOB = DateOfBirthTB.Text;
+            String gender = GenderList.SelectedValue.ToString();
+            int genderListIndex = GenderList.SelectedIndex;
 
-            int newID = 0;
-            int lastID = userRepo.GetLastUserID();
+            ErrorLbl.Text = AuthController.doRegister(username, password, email, DOB, genderListIndex, confirmPassword);
 
-            if (lastID == null)
+            if (ErrorLbl.Text.Equals("Success"))
             {
-                lastID = 1;
+                UserHandler.doRegister(username, password, email, DOB, gender);
+                Response.Redirect("~/Views/Login.aspx");
             }
-            else
-            {
-                lastID++;
-            }
-
-            newID = lastID;
-
-            return newID;
-        }
-
-        private void RegisterUser()
-        {
-            UserRepository userRepo = new UserRepository();
-
-            int newID = GenerateUserID();
-            String newUsername = UsernameTB.Text.ToString();
-            String newEmail = EmailTB.Text.ToString();
-            DateTime newDOB = Convert.ToDateTime(DateOfBirthTB.Text);
-            String newGender = GenderList.SelectedValue.ToString();
-            String newRole = "Customer";
-            String newPassword = ConfirmPassTB.Text.ToString();
-
-            userRepo.InsertUser(newID, newUsername, newEmail, newDOB, newGender, newRole, newPassword);
-            Response.Redirect("~/Views/Login.aspx");
-        }
-
-        private bool IsAlphanumeric(String password)
-        {
-            // Regular expression to check if the password is alphanumeric
-            String pattern = @"^[a-zA-Z0-9]+$";
-            Regex regex = new Regex(pattern);
-            return regex.IsMatch(password);
         }
     }
 }
